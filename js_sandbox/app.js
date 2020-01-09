37c3d5abb294508a1ba9e55e1a33b9233f447446
My Tran <myytran28@gmail.com>
2020-01-08 19:57:54 -0500

    Primitive and Reference Data Types

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,36 +1,87 @@
-//log to console
-console.log('hello world');
-console.log(123);
-console.log(true);
-var greeting = 'hello';
-console.log(greeting);
+// //log to console
+// console.log('hello world');
+// console.log(123);
+// console.log(true);
+// var greeting = 'hello';
+// console.log(greeting);
 
-//array
-console.log([1, 2, 3, 4]);
+// //array
+// console.log([1, 2, 3, 4]);
 
-//object literal
-console.log({ a: 1, b: 2, c: 3 });
-//print out object in form of a table
-console.table({ a: 1, b: 2, c: 3 });
+// //object literal
+// console.log({ a: 1, b: 2, c: 3 });
+// //print out object in form of a table
+// console.table({ a: 1, b: 2, c: 3 });
 
-//consoles and shows where error occurs in code
-console.error('this is some error');
+// //consoles and shows where error occurs in code
+// console.error('this is some error');
 
-//clear console
-console.clear();
-console.warn('this is a warning');
+// //clear console
+// console.clear();
+// console.warn('this is a warning');
 
-//Timer for selected code ran
-console.time('hello');
-console.log(123);
-console.log(123);
-console.log(123);
-console.log(123);
-console.timeEnd('hello');
+// //Timer for selected code ran
+// console.time('hello');
+// console.log(123);
+// console.log(123);
+// console.log(123);
+// console.log(123);
+// console.timeEnd('hello');
 
-// Variables and Declarations
+// // Variables and Declarations
 
-//  var, let, const
+// //  var, let, const
 
-var name = 'John Doe';
-console.log(name);
+// var name = 'John Doe';
+// console.log(name);
+
+// //you can reassign variables when you use var or let
+
+// //init var
+// var greeting;
+// console.log(greeting); //Undefined
+// greeting = 'Hello';
+// console.log(greeting); //Hello
+
+// LET
+// let name = 'John Doe';
+// console.log(name); //John Doe
+// name = 'Steve Smith';
+// console.log(name); //Steve Smith
+
+// CONST [can't change or be reassigned]
+// have to assign a value
+
+//PRIMITIVE TYPES
+
+// //string
+// const name = 'John Doe';
+
+// //number
+// const age = 46;
+
+// //Boolean
+// const hasKids = true;
+
+// //null
+// const car = null;
+
+// //undefined
+// let test;
+
+// //Symbol
+// const sym = Symbol();
+
+// // REFERENCE TYPES
+// //array
+// const hobbies = ['movies', 'music'];
+
+// //object literal
+// const address = {
+//   city: 'Boston',
+//   state: 'MA'
+// };
+// const today = new Date();
+
+// console.log(today);
+// console.log(typeof today);
