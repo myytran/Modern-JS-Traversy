1a72836ad354ce234ca95b7178fa002d55dbe990
My Tran <myytran28@gmail.com>
2020-01-13 15:26:12 -0500

    type conversions

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -85,3 +85,46 @@
 
 // console.log(today);
 // console.log(typeof today);
+
+// TYPE CONVERSION
+
+let val;
+//number to string
+val = String(555);
+val = String(4 + 4);
+
+//boolean to string
+val = String(true);
+
+//Date to string
+val = String(new Date());
+
+// Array to string
+val = String([1, 2, 3, 4]);
+
+// toString() method
+
+val = (5607987).toString();
+val = true.toString();
+
+//String to number
+val = Number('6');
+val = Number(true); //1
+val = Number(false); //0
+val = Number(null); //0
+
+val = parseInt('299.4'); //299
+val = parseFloat('299.4'); //299.4
+
+// // output
+// console.log(val);
+// console.log(typeof val);
+// //console.log(val.length);
+// console.log(val.toFixed());
+
+const val1 = String('8');
+const val2 = 9;
+const sum = val1 + val2;
+
+console.log(sum);
+console.log(typeof sum);
